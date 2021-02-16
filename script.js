e3800a2db70df01a51b1e92a67ad4b6182f601b6
Beccabokka <79112860+Beccabokka@users.noreply.github.com>
2021-02-16 00:27:43 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+// Screen Inputs
+var mainInput = document.getElementsByClassName("input")[0];
+var formulaInput = document.getElementsByClassName("formula")[0];
+
+// Function buttons
+var ac = document.getElementById("AC");
+var ce = document.getElementById("CE");
+var equals = document.getElementById("equals");
+
+// Screen Variables
+var main = "0"
+var formula = "0"
+
+//Functions
+var setMainInput = function (value) {
+  if (main === '0' && value != '.') {
+    main = "";
+  }
+
+  setMain(main + value)
+}
+
+var canUpdateMain = function(value) {
+  if (value === '.' && main.indexOf('.') >= 0) {
+    return false;
+  }
+
+  if (main.length >= 7)  {
+    return false;
+  }
+
+  return true;
+}
+
+var setFormulaInput = function (value) {
+
+  if (value === ' ÷ ' || value === ' + ' || value === ' − ' || value === ' × ' ) {
+    main = '0';
+    mainInput.innerHTML = main;
+  }
+
+  if (formula === '0' && value != '.' && value != ' + ' && value != ' × ' && value != ' ÷ ') {
+    formula = "";
+  }
+
+  if (formula.length >= 30)  {
+    formulaInput.innerHTML = "DIGIT LIMIT REACHED"
+    return;
+  }
+
+  setFormula(formula + value)
+}
+
+var resetMain = function () {
+  setFormula(formula.slice(0, -main.length));
+
+  if (formula === '') {
+    setFormula('0')
+  }
+
+  setMain('0')
+}
+
+var setFormula = function (str) {
+  formula = str;
+  formulaInput.innerHTML = formula;
+}
+
+var setMain = function (str) {
+  main = str;
+  mainInput.innerHTML = main;
+}
+
+var calculateFormula = function () {
+  var calculated = eval(formula.replace('÷', '/').replace('−', '-').replace('×', '*'));
+
+  calculated = calculated.toString()
+
+  setFormula(calculated)
+  setMain(calculated)
+}
+
+// Keypad Events
+ac.onclick = function() {
+  resetMain();
+  setFormula('0')
+}
+
+ce.onclick = function() {
+  resetMain();
+}
+
+equals.onclick = function() {
+  calculateFormula();
+}
+
+var symbolPadPress = function (symbol) {
+  if (formula[formula.length - 1] === ' ') {
+    return;
+  }
+
+  if (formula === ' + ' && formula === ' ÷ ' && formula === ' × ') {
+    return;
+  }
+
+  setFormulaInput(symbol);
+}
+
+var padPress = function(number) {
+  if (canUpdateMain(number)) {
+    setMainInput(number);
+    setFormulaInput(number);
+  }
+}
+
+// setting initial values - "Turning on calculator"
+setMain('0')
+setFormula('0')
